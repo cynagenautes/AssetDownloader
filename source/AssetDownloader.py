d05aa6ab1774629152006ba0a342992a048334ab
MayaYamato <gbp.0516@gmail.com>
2019-02-09 21:46:34 +0900

    AssetDownloaderとBgmDownloaderの機能を統合

--- source/AssetDownloader.py
+++ source/AssetDownloader.py
@@ -1,15 +1,18 @@
 import os
+import requests
 import urllib.error
 import urllib.request
 
 #ダウンロードディレクトリの設定
-download_dir = 'asset'
+download_dir_asset = 'asset'
+download_dir_bgm = 'bgm'
 
 #ダウンロードディレクトリがなければ新規作成する
-if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
+if not os.path.exists(download_dir_bgm):
+        os.makedirs(download_dir_bgm)
+if not os.path.exists(download_dir_asset):
+        os.makedirs(download_dir_asset)
 
-#ダウンロード関数の定義
 def download_asset(url, dst_path):
     try:
         data = urllib.request.urlopen(url).read()
@@ -18,32 +21,54 @@ def download_asset(url, dst_path):
     except urllib.error.URLError as e:
         print(e)
 
-#一行ずつ繰り返し処理
-print('AssetBundleInfoを読み込みます')
-with open('AssetBundleInfo') as lines:
-    for line in lines:
-        #折り返し,改行文字の削除
-        url = line.rstrip('\r\n')
-        #ファイルの保存名の設定
-        filename = os.path.basename(url)
-        #pathの結合 asset/+filename
-        dst_path = os.path.join(download_dir, filename)
-        print(url)
-        #既存ファイルの存在判定
-        if os.path.exists(dst_path):
-            #10000回までfor同一ファイル保存を回避できる
-            for n in range (1, 10000):
-                #保存名をfilename+(n)に変更
-                new_filename = str(filename)  + '(' + str(n) + ')'
-                #dst_pathの再設定(ファイルネームの変更)
-                dst_path = os.path.join(download_dir, new_filename)
+def download_bgm(url,dst_path):
+    f5 = open(dst_path,'wb')
+    download = requests.get(url)
+    f5.write(download.content)
+    f5.close()
 
-                if not os.path.exists(dst_path):
-                    download_asset(url, dst_path)
-                    break
+tmp = input("Which do you want, music or asset? asset = 1 music = 2 ⇒　").rstrip()
 
-                else:
-                    #既に重複ファイル回避がされていた場合さらにforを回す必要がある
-                    continue
-        else:
-            download_asset(url, dst_path)
+if int(tmp) == 1:
+
+    print('Loading file')
+    with open('AssetBundleInfo') as lines:
+        for line in lines:
+            #折り返し,改行文字の削除
+            url = line.rstrip('\r\n')
+            #ファイルの保存名の設定
+            filename = os.path.basename(url)
+            #pathの結合 asset/+filename
+            dst_path = os.path.join(download_dir_asset, filename)
+            print(url)
+            #既存ファイルの存在判定
+            if os.path.exists(dst_path):
+                #10000回までfor同一ファイル保存を回避できる
+                for n in range (1, 10000):
+                    #保存名をfilename+(n)に変更
+                    new_filename = str(filename)  + '(' + str(n) + ')'
+                    #dst_pathの再設定(ファイルネームの変更)
+                    dst_path = os.path.join(download_dir_asset, new_filename)
+
+                    if not os.path.exists(dst_path):
+                        download_asset(url, dst_path)
+                        break
+                    else:
+                        #既に重複ファイル回避がされていた場合さらにforを回す必要がある
+                        continue
+            else:
+                download_asset(url, dst_path)
+
+if int(tmp) == 2:
+
+    print('Loading file')
+    with open('BgmLists') as lines:
+        for line in lines:
+            #折り返し,改行文字の削除
+            url = line.rstrip('\r\n')
+            #ファイルの保存名の設定
+            filename = os.path.basename(url)
+            #pathの結合 asset/+filename
+            dst_path = os.path.join(download_dir_bgm, filename)
+            print(url)
+            download_bgm(url, dst_path)
